da540b90f74a49460108e59631fd3f18ba0d8a56
JosephinAngegardien <stephane.issenlor@gmail.com>
2019-07-18 17:03:00 +0200

    je sais pas j'ai pas tout compris

--- src/Form/StagiaireType.php
+++ src/Form/StagiaireType.php
@@ -19,7 +19,11 @@ class StagiaireType extends AbstractType
             ->add('nom',TextType::class)
             ->add('prenom',TextType::class)
             ->add('sexe',ChoiceType::class, ["choices"=>["Homme"=>"H", "Femme"=>"F"], "expanded"=>true])
-            ->add('dateNaissance',DateType::class)
+            ->add('dateNaissance',DateType::class, [
+                "years"=>range(date("Y")-7, date("Y")-77),
+                "label"=>"Date de naissance",
+                "format"=>"ddMMMMyyyy"
+            ])
             ->add('ville',TextType::class, ["required" => false])
             ->add('email',TextType::class)
             ->add('telephone',TextType::class)
